45dc35053554b272bd6ef438d839842161316ee0
camille <camille.lazennec@ynov.com>
2026-02-12 02:27:54 +0100

    feat: add AppBar with logo and pink background

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Pour test rapide, on met les clés directement
   await Supabase.initialize(
     url: 'https://frtqyiyjxgunaclttrbt.supabase.co',
     anonKey: 'sb_publishable_1rVx7Ibwn-UmrHPdnigFpg_r2ADUIrE',
@@ -23,15 +25,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
+          seedColor: const Color(0xFFEC6A6D), // Couleur rgb(236,106,109)
         ),
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
+          backgroundColor: Color(0xFFEC6A6D),
         ),
       ),
-
-      home: const HomePage(), //première page affichée = HomePage.
+      home: const HomePage(),
     );
   }
 }
@@ -43,10 +43,18 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Budiz'),
+        centerTitle: true,
+        // Logo dans l'AppBar
+        title: Image.asset(
+          'assets/logo.jpg', // Assurez-vous que le fichier existe ici
+          height: 40,
+        ),
       ),
       body: const Center(
-        child: Text('Connexion Supabase réussie 🚀'),
+        child: Text(
+          'Connexion Supabase réussie 🚀',
+          style: TextStyle(fontSize: 18),
+        ),
       ),
     );
   }
